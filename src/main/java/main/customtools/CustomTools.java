52047dfd3a4243d422a903384ab12548b387d96c
thor12022 <thor12022@gmail.com>
2014-12-07 23:49:00 -0600

    Add Support for custom Axes and Shovels

--- src/main/java/main/customtools/CustomTools.java
+++ src/main/java/main/customtools/CustomTools.java
@@ -17,7 +17,9 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import main.customtools.client.gui.CreativeTabBaseMod;
 import main.customtools.client.gui.GuiHandler;
+import main.customtools.items.ItemCustomAxe;
 import main.customtools.items.ItemCustomPick;
+import main.customtools.items.ItemCustomShovel;
 import main.customtools.proxies.CommonProxy;
 import main.customtools.util.EventHandler;
 import main.customtools.util.TextHelper;
@@ -41,6 +43,8 @@ public class CustomTools
    public static Logger logger = LogManager.getLogger(ModInformation.NAME);
    public static Configuration config;
    public static Item[] pickaxes;
+   public static Item[] axes;
+   public static Item[] shovels;
    
    @Mod.Instance
    public static CustomTools instance;
@@ -52,15 +56,15 @@ public class CustomTools
 
       config = new Configuration(event.getSuggestedConfigurationFile());
 
-      int toolCount = config.get("pickaxes", "pickaxeCount", 1, "Number of pickaxes added", 0, 32).getInt();
-      
-      pickaxes = new Item[32];
-      
       String name, texture;
       int harvestLevel, maxUses, enchantability;
       float efficiency, damage;
       
-      for( int count = 0; count < toolCount; ++count)
+      int pickaxeCount = config.get("pickaxes", "pickaxeCount", 1, "Number of pickaxes added", 0, 32).getInt();
+      
+      pickaxes = new Item[pickaxeCount];
+            
+      for( int count = 0; count < pickaxeCount; ++count)
       {
          name = config.get("pickaxes.pickaxe"+count, "name", "Custom Pickaxe", "Displayed name of Pickaxe").getString();
          texture = config.get("pickaxes.pickaxe"+count, "texture", "customtools:customPickaxe", "Texture name of pickaxe").getString();
@@ -75,6 +79,48 @@ public class CustomTools
          Item.ToolMaterial toolMaterial = EnumHelper.addToolMaterial("customPickaxe"+count, harvestLevel, maxUses, efficiency, damage, enchantability);
          pickaxes[count] = new ItemCustomPick(toolMaterial, name, texture);
          GameRegistry.registerItem(pickaxes[count], "customPickaxe"+count);
+      }
+      
+      int axeCount = config.get("axes", "axeCount", 1, "Number of axes added", 0, 32).getInt();
+      
+      axes = new Item[axeCount];
+            
+      for( int count = 0; count < axeCount; ++count)
+      {
+         name = config.get("axes.axe"+count, "name", "Custom Axe", "Displayed name of axe").getString();
+         texture = config.get("axes.axe"+count, "texture", "customtools:customaxe", "Texture name of axe").getString();
+         
+         harvestLevel = config.get("axes.axe"+count, "harvestLevel", 0, "Harvest Level of axe; 3=DIAMOND, 2=IRON, 1=STONE, 0=WOOD/GOLD", 0, 16).getInt();
+         maxUses = config.get("axes.axe"+count, "maxUses", -1, "Max uses of axe; wood=59, stone=131, iron=250, diamond=1561, gold=32").getInt();
+         enchantability = config.get("axes.axe"+count, "enchantability", 32, "Enchantability of axe; Diamond=??, Iron=??, Stone=??, Wood=??, Gold=??").getInt();
+         
+         efficiency = (float)config.get("axes.axe"+count, "efficiency", 3.0f, "Efficiency of axe").getDouble();
+         damage = config.get("axes.axe"+count, "damage", 1, "Damage against entities").getInt();
+         
+         Item.ToolMaterial toolMaterial = EnumHelper.addToolMaterial("customAxe"+count, harvestLevel, maxUses, efficiency, damage, enchantability);
+         axes[count] = new ItemCustomAxe(toolMaterial, name, texture);
+         GameRegistry.registerItem(axes[count], "customAxe"+count);
+      }
+      
+      int shovelCount = config.get("shovels", "shovelCount", 1, "Number of shovels added", 0, 32).getInt();
+      
+      shovels = new Item[shovelCount];
+            
+      for( int count = 0; count < shovelCount; ++count)
+      {
+         name = config.get("shovels.shovel"+count, "name", "Custom Shovel", "Displayed name of shovel").getString();
+         texture = config.get("shovels.shovel"+count, "texture", "customtools:customshovel", "Texture name of shovel").getString();
+         
+         harvestLevel = config.get("shovels.shovel"+count, "harvestLevel", 0, "Harvest Level of shovel; 3=DIAMOND, 2=IRON, 1=STONE, 0=WOOD/GOLD", 0, 16).getInt();
+         maxUses = config.get("shovels.shovel"+count, "maxUses", -1, "Max uses of shovel; wood=59, stone=131, iron=250, diamond=1561, gold=32").getInt();
+         enchantability = config.get("shovels.shovel"+count, "enchantability", 32, "Enchantability of shovel; Diamond=??, Iron=??, Stone=??, Wood=??, Gold=??").getInt();
+         
+         efficiency = (float)config.get("shovels.shovel"+count, "efficiency", 3.0f, "Efficiency of shovel").getDouble();
+         damage = config.get("shovels.shovel"+count, "damage", 1, "Damage against entities").getInt();
+         
+         Item.ToolMaterial toolMaterial = EnumHelper.addToolMaterial("customShovel"+count, harvestLevel, maxUses, efficiency, damage, enchantability);
+         shovels[count] = new ItemCustomShovel(toolMaterial, name, texture);
+         GameRegistry.registerItem(shovels[count], "customShovel"+count);
       }
       
       MinecraftForge.EVENT_BUS.register(new EventHandler());
