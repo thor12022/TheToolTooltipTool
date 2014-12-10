bd1d12be9a5e772a6cf1f86b7bb17f1d5c180056
thor12022 <thor12022@gmail.com>
2014-12-09 22:01:59 -0600

    Issue #1: Add support for Efficiency & Unbreaking enchantment modifiers to stats

--- src/main/java/main/thetooltooltiptool/ModInformation.java
+++ src/main/java/main/thetooltooltiptool/ModInformation.java
@@ -11,6 +11,6 @@ public class ModInformation
    public static final String ID = "thetooltooltiptool";
    public static final String CHANNEL = "TheToolTooltipTool";
    public static final String DEPEND = "";
-   public static final String VERSION = "0.0.0";
+   public static final String VERSION = "0.2.0";
    public static final String CLIENTPROXY = "main.thetooltooltiptool.proxies.ClientProxy";
 }
--- src/main/java/main/thetooltooltiptool/client/gui/TooltipHelper.java
+++ src/main/java/main/thetooltooltiptool/client/gui/TooltipHelper.java
@@ -9,6 +9,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockStone;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -18,9 +20,20 @@ public class TooltipHelper
 {
    public static final void AppendTooltip(ItemStack itemStack, List<String> tooltip)
    {
-       tooltip.add(new String(TextHelper.LIGHT_GRAY + 
-                              TextHelper.localize(ModInformation.ID + ".tooltip.maxDamage")+": " + 
-                              itemStack.getItem().getMaxDamage()));
+       int maxDamageMultiplier = EnchantmentHelper.getEnchantmentLevel(Enchantment.unbreaking.effectId, itemStack) + 1;
+       if( maxDamageMultiplier == 1)
+       {
+          tooltip.add(new String(TextHelper.LIGHT_GRAY + 
+                                 TextHelper.localize(ModInformation.ID + ".tooltip.maxDamage")+": " + 
+                                 itemStack.getItem().getMaxDamage()));
+       }
+       else
+       {
+          tooltip.add(new String(TextHelper.LIGHT_GRAY + 
+                TextHelper.localize(ModInformation.ID + ".tooltip.averageMaxDamage")+": " + 
+                itemStack.getItem().getMaxDamage() * maxDamageMultiplier));
+       }
+       float extraEfficiency = ( EnchantmentHelper.getEnchantmentLevel(Enchantment.efficiency.effectId, itemStack) * 0.3f ) + 1;
        Set<String> toolClasses = itemStack.getItem().getToolClasses(itemStack);
        for(String toolClass : toolClasses)
        {
@@ -29,14 +42,14 @@ public class TooltipHelper
                                  " (" + TextHelper.localize(ModInformation.ID + ".tooltip." + toolClass) + "): " + 
                                  itemStack.getItem().getHarvestLevel(itemStack, toolClass)));
           Block digBlock;
-          if( toolClass.equals("pickaxe") )
+          if( toolClass.equals("pickaxe") || toolClass.equals("paxel") )
           {
              digBlock = (BlockStone)Block.blockRegistry.getObject("stone");
              //! @todo This should use the metadata of the tool
              tooltip.add(new String(TextHelper.LIGHT_GRAY + 
                                     TextHelper.localize(ModInformation.ID + ".tooltip.digSpeed") + 
                                     " (" + TextHelper.localize(ModInformation.ID + ".tooltip." + toolClass) + "): " + 
-                                    itemStack.getItem().getDigSpeed(itemStack, digBlock,0)));
+                                    String.format("%.1f", itemStack.getItem().getDigSpeed(itemStack, digBlock,0) * extraEfficiency)));
           }
           else if( toolClass.equals("shovel") )
           {
@@ -45,7 +58,7 @@ public class TooltipHelper
              tooltip.add(new String(TextHelper.LIGHT_GRAY + 
                                     TextHelper.localize(ModInformation.ID + ".tooltip.digSpeed") +
                                     " (" + TextHelper.localize(ModInformation.ID + ".tooltip." + toolClass) + "): " + 
-                                    itemStack.getItem().getDigSpeed(itemStack, digBlock,0)));
+                                    String.format("%.1f", itemStack.getItem().getDigSpeed(itemStack, digBlock,0) * extraEfficiency)));
           }else if( toolClass.equals("axe") )
           {
              digBlock = (BlockLog)Block.blockRegistry.getObject("log");
@@ -53,7 +66,7 @@ public class TooltipHelper
              tooltip.add(new String(TextHelper.LIGHT_GRAY + 
                                     TextHelper.localize(ModInformation.ID + ".tooltip.digSpeed") +
                                     " (" + TextHelper.localize(ModInformation.ID + ".tooltip." + toolClass) + "): " + 
-                                    itemStack.getItem().getDigSpeed(itemStack, digBlock,0)));
+                                    String.format("%.1f", itemStack.getItem().getDigSpeed(itemStack, digBlock,0) * extraEfficiency)));
           } 
        }       
        tooltip.add(new String(TextHelper.LIGHT_GRAY + 
